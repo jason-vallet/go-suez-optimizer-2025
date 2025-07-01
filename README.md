0b4b12010e4544e8e90adef590b2957eb8a7efca
jason.vallet <jason.vallet>
2025-07-01 19:33:13 +0200

    Push first version of the script

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # go-suez-optimizer-2025
-A small CLI-tool to help the Suez employees
+
+A small CLI-tool to help the French Suez employees pick the best amounts to subscribe to *Classic* or *Multiple*. 
+
+The [Go Suez](https://go.suez.com) operation runs from July 1st to July 15th.
+
+## How to use?
+
+The script must be run with *python*. It will display a proposal of amounts to distribute between the two plans as well as some additionnal information regarding the corresponding interest rate and the total gain in 5 years.
+
+You can use the following options to customise the behaviour of the tool:
+
+- `-s`, `--salary`: This is your raw yearly income before tax (default: 25000)
+- `-i`, `--investment`: Specify the amount of money you want to invest (default: 2000)
+- `-p`, `--price`: Indicate the bond price estimated in 5 years (default: 1.4)
+
+Consult the help (option is `-h` or `--help`) for more details.
+
+
+## Examples
+
+1. You are a Junior developer earning 25k€ per year, you want to invest at most 2k€, and you believe the share price will rise to reach 1.45€ in 5 years.
+
+You case use the following command:
+
+```
+python .\script.py -i 2000 -s 25000 -p 1.45
+```
+
+Which will output the following information:
+
+```
+~=< Suez Go Optimizer >=~
+You should invest as follows:
+- Classic: 1513.00€
+        result: 2684.86€ (12.15%)
+- Multiple: 487.00€
+        result: 981.85€ (15.05%)
+- Total: 3666.71€ (+1666.71€ @ 12.89%)
+```
+
+2. You are a manager of marketing earning 100k€ per year, you want to invest at most 8k€, and you believe the share price will rise to reach 1.32€ in 5 years.
+
+You case use the following command:
+
+```
+python .\script.py -i 8000 -s 100000 -p 1.32
+```
+
+Which will output the following information:
+
+```
+~=< Suez Go Optimizer >=~
+You should invest as follows:
+- Classic: 6051.00€
+        result: 8434.31€ (6.87%)
+- Multiple: 1949.00€
+        result: 2703.45€ (6.76%)
+- Total: 11137.76€ (+3137.76€ @ 6.84%)
+```
+
+3. You are a VP of customer relations earning 300k€ per year, you want to invest at most 15k€, and you believe the share price will rise to reach 1.1€ in 5 years.
+
+You case use the following command:
+
+```
+python .\script.py -i 15000 -s 300000 -p 1.1
+```
+
+Which will output the following information:
+
+```
+~=< Suez Go Optimizer >=~
+You should invest as follows:
+- Classic: 15000.00€
+        result: 16872.49€ (2.38%)
+- Multiple: 0.00€
+        result: 0.00€ (nan%)
+- Total: 16872.49€ (+1872.49€ @ 2.38%)
+```
---
+++ script.py
@@ -0,0 +1,143 @@
+import json
+import sys
+import warnings
+
+import numpy as np
+
+__version__ = "0.0.1"
+warnings.filterwarnings("ignore")
+
+def calcul_invest_classic(var_multiple, max_invest):
+    ratio_classic = 1
+    ratio_multiple = 9.72
+    var_classic = (max_invest - var_multiple * ratio_multiple)
+    return var_classic/ratio_classic
+
+
+def calcul_invest_multiple(var_classic, max_invest):
+    ratio_classic = 1
+    ratio_multiple = 9.72
+    var_multiple = (max_invest - var_classic * ratio_classic)
+    return var_multiple/ratio_multiple
+
+
+# go classic
+## abondement
+def abondement(var_classic):
+    abondement = 0
+    if var_classic <= 250:
+        abondement = var_classic*1
+    else:
+        if var_classic <= 500:
+            abondement = 250 + (var_classic - 250)*0.5
+        else:
+            abondement = 375
+    return abondement
+
+## resulat classic
+def calcul_classic(var_classic, action_ref, action_final, target_year):
+    res_abondement = abondement(var_classic)
+    invest_classic = var_classic
+    invest_classic += res_abondement * (1-0.097) # CSG / CRDS à 9.7%
+    invest_classic *= action_ref
+    res_classic = (invest_classic/action_ref)*action_final
+    gain_classic = res_classic - var_classic
+    interet_classic = (res_classic/var_classic)**(1/target_year)
+    interet_classic -= 1
+    interet_classic *= 100
+    return {'result': res_classic, 'gain': gain_classic, 'interet': interet_classic, 'var': var_classic}
+
+
+# go multiple
+def calcul_multiple(var_multiple, action_ref, action_final, target_year):
+    gain_multiple = (var_multiple / action_ref) * action_final
+    gain_multiple -= var_multiple
+    gain_multiple *= 6
+    res_multiple = var_multiple + gain_multiple
+    interet_multiple = (res_multiple/var_multiple)**(1/target_year)
+    interet_multiple -= 1
+    interet_multiple *= 100
+    return {'result': res_multiple, 'gain': gain_multiple, 'interet': interet_multiple, 'var': var_multiple}
+
+
+def calcul_total(var_classic, var_multiple, action_ref, action_final, target_year):
+    res_classic = calcul_classic(var_classic, action_ref, action_final, target_year)
+    res_multiple = calcul_multiple(var_multiple, action_ref, action_final, target_year)
+    res_total = res_classic['result'] + res_multiple['result']
+    gain_total = res_classic['gain'] + res_multiple['gain']
+    interet_total = (res_total/(var_classic+var_multiple))**(1/target_year)
+    interet_total -= 1
+    interet_total *= 100
+    return {'result': res_total, 'gain': gain_total, 'interet': interet_total, 'classic': res_classic, 'multiple': res_multiple}
+
+
+#res_classic, gain_classic, interet_classic = calcul_classic(var_classic, action_ref, action_final, target_year)
+#res_multiple, gain_multiple, interet_multiple = calcul_multiple(var_multiple, action_ref, action_final, target_year)
+#res_total, gain_total, interet_total = calcul_total(var_classic, var_multiple, action_ref, action_final, target_year)
+
+def ff(v):
+    return v['gain']
+
+if __name__ == '__main__':
+    remuneration = 25000
+    invest = 2000
+    action_ref = 1.24
+    action_final = 1.4
+
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog='Go Suez Optimizer 2025',
+        description='''
+            Become rich by being lazy! This program computes for you the best way to invest your money with Go Suez. It's basically like free financial consulting (well, not quite ^<^).
+            ''',
+        epilog='=== SUEZ 2025 ===',
+        exit_on_error=True,
+        add_help=False
+    )
+    # tools
+    parser.add_argument('-h', '--help',
+        action='help', 
+        help="Display this message")
+    parser.add_argument('-v', '--version', 
+        action='version', 
+        help="Script version",
+        version=__version__)
+    
+    # optional
+    parser.add_argument('-s', '--salary', 
+        type=int, 
+        help="This is your raw yearly income before tax (default: 25000)")
+    parser.add_argument('-i', '--investment',
+        type=int,
+        help="Specify the amount of money you want to invest (default: 2000)")
+    parser.add_argument('-p', '--price',
+        type=float,
+        help="Indicate the bond price estimated in 5 years (default: 1.4)")
+
+    args = parser.parse_args()
+    if args.salary:
+        remuneration = int(args.salary)
+    if args.investment:
+        invest = int(args.investment)
+    if args.price:
+        action_final = float(args.price)
+
+    target_year = 5
+    max_invest = remuneration * 0.25
+
+    vals_multiple = np.arange(0, min(calcul_invest_multiple(0, max_invest), invest))
+    vals_classic = [min(calcul_invest_classic(i, max_invest), invest-i) for i in vals_multiple]
+
+    res = []
+    for i in range(len(vals_classic)):
+        res.append(calcul_total(vals_classic[i], vals_multiple[i], action_ref, action_final, target_year))
+
+    #print(json.dumps(max(res, key=ff), indent=2))
+    result = max(res, key=ff)
+    print("~=< Suez Go Optimizer >=~")
+    print("You should invest as follows:")
+    print("- Classic: {:.2f}€".format(result['classic']['var']))
+    print('\tresult: {:.2f}€ ({:.2f}%)'.format(result['classic']['result'], result['classic']['interet']))
+    print("- Multiple: {:.2f}€".format(result['multiple']['var']))
+    print('\tresult: {:.2f}€ ({:.2f}%)'.format(result['multiple']['result'], result['multiple']['interet']))
+    print("- Total: {:.2f}€ (".format(result['result'])+('+' if result['gain']>=0 else '-')+'{:.2f}€ @ {:.2f}%)'.format(result['gain'], result['interet']))
